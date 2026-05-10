753093ce6831e2a5481297f1c73a1f938f4bff7d
whyavya710 <whyavya@gmail.com>
2026-05-10 10:18:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import os
+import streamlit as st
 
-BASE_DIR = os.path.dirname(__file__)
-
-df_path = os.path.join(BASE_DIR, "co2_emissions_kt_by_country.csv")
-fuel_path = os.path.join(BASE_DIR, "fossil_fuel_co2_emissions-by-nation_with_continent.csv")
-
-df = pd.read_csv(df_path)
-fuel = pd.read_csv(fuel_path)
+st.write("Files in app folder:", os.listdir())
